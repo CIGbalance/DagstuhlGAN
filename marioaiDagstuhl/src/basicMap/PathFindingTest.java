dc693831993a36cd2e15c67a5d338a0dddc0b9dc
Jialin Liu <ljialin@users.noreply.github.com>
2017-11-23 15:20:25 +0000

    add basic map

---
+++ marioaiDagstuhl/src/basicMap/PathFindingTest.java
@@ -0,0 +1,9 @@
+package basicMap;
+
+public class PathFindingTest {
+    public static void main(String[] args) {
+        Map map = new Map(5,4, "sample.txt");
+        System.out.println(map.solvable());
+
+    }
+}
