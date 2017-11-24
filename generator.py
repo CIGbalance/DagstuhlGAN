3425f5739c1e7e57038d31b1aaaade39b2cfbf3b
Adam M. Smith <adam@adamsmith.as>
2017-11-24 10:28:10 +0100

    Support efficient batch generation in generator.py

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 
 # This generator program expands a low-dimentional latent vector into a 2D array of tiles.
 # Usage: generator.py architecture.json weights.h5 < z.jsons > levels.jsons
+# Each line of input should be an array of z vectors (which are themselves arrays of floats -1 to 1)
+# Each line of output is an array of levels (which are arrays-of-arrays of integer tile ids)
 
 import sys
 import json
@@ -14,6 +16,6 @@ if __name__ == '__main__':
 	model.load_weights(weights_filename, True)
 
 	for line in sys.stdin.readlines():
-		z = numpy.array([json.loads(line)])
-		level = model.predict(z).argmin(-1)[0]
+		zs = numpy.array(json.loads(line))
+		levels = model.predict(zs).argmin(-1)
 		print(json.dumps(level.tolist()))
